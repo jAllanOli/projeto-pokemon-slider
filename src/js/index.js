b32a1fdf559830b6a2c686e942be49940a52e33b
Allan Oliveira <98463818+jAllanOli@users.noreply.github.com>
2022-07-31 14:43:21 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+const btnAvancar = document.getElementById('btn-foward');
+const btnVoltar = document.getElementById('btn-back');
+const cards = document.querySelectorAll('.card');
+let cardDisplayed = 0;
+
+function hideSelectedCard() {
+    const cardSelected = document.querySelector('.selected');
+    cardSelected.classList.remove('selected');
+}
+
+function showCard(cardIndex) {
+    cards[cardIndex].classList.add('selected');
+}
+
+btnAvancar.addEventListener('click', function () {
+    if (cardDisplayed >= 2) {
+        cardDisplayed = -1;
+    }
+    if (cardDisplayed === cards.length - 1) return
+
+    hideSelectedCard();
+
+    cardDisplayed++;
+    showCard(cardDisplayed);
+})
+
+btnVoltar.addEventListener('click', function () {
+    if (cardDisplayed <= 0) {
+        cardDisplayed = 4;
+    }
+    if (cardDisplayed === 0) return;
+
+    hideSelectedCard();
+
+    cardDisplayed--;
+    showCard(cardDisplayed);
+})
+
